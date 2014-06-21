70e759c6946dd785fd26e9a5e8d2529e334607ae
Katharina Sabel <sabel.katharina@gmail.com>
2014-06-21 14:19:26 +0200

    Improxing remove script and fixing the "pseudo-fix" of the build script.

--- build.py
+++ build.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python
 
+'''
+
+Copyright (c) 2014 Katharina Sabel <katharina.sabel@2rsoftworks.de>
+Copyright (c) 2014 Random Robot Softworks
+www.katharinasabel.de | www.2rsoftworks.de
+
+Licensed under the Apache License, Version 2.0 (the "License");		
+you may not use this file except in compliance with the License.		
+You may obtain a copy of the License at	
+
+		http://www.apache.org/licenses/LICENSE-2.0						
+									
+Unless required by applicable law or agreed to in writing, software	
+distributed under the License is distributed on an "AS IS" BASIS,		
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Found a bug? Report it in the repository issue tracker:
+
+		https://github.com/SpaceKookie/Poke
+
+'''
+
+
 from datetime import datetime
 from subprocess import Popen, PIPE, call, check_call, CalledProcessError
 from sys import exit
@@ -24,14 +49,14 @@ else:
 	exit()
 
 print("Downloading compiler files...")
-
-
 try:
-        check_call(['wget'])
+	check_call(['wget'])
 except CalledProcessError:
-        call("wget 'https://pypi.python.org/packages/source/P/PyInstaller/PyInstaller-2.1.tar.gz' -o 'pyinstall.tar.gz'", shell=True)
+	print "Using wget"
+	call("wget https://pypi.python.org/packages/source/P/PyInstaller/PyInstaller-2.1.tar.gz -O pyinstall.tar.gz", shell=True)
 except OSError:
-        call("curl 'https://pypi.python.org/packages/source/P/PyInstaller/PyInstaller-2.1.tar.gz' -o 'pyinstall.tar.gz'", shell=True)
+	print "Using curl"
+	call("curl 'https://pypi.python.org/packages/source/P/PyInstaller/PyInstaller-2.1.tar.gz' -o 'pyinstall.tar.gz'", shell=True)
 
 call("mkdir ./pyinstall && tar -vxzf pyinstall.tar.gz -C pyinstall --strip-components 1", shell=True)
 
@@ -39,30 +64,40 @@ print("Configuring application build file to system")
 
 call("python pyinstall/utils/makespec.py -n poke -F source/*.py", shell=True)
 
+print("\n")
 print("Compiling application")
-
 call("python pyinstall/pyinstaller.py poke.spec", shell=True)
 
+print("\n")
 print("Moving application to '/usr/bin'. THIS REQUIRES ROOT PRIVILEGES!")
 
 call("chmod +x dist/poke", shell=True)
 call("sudo mv dist/poke /usr/bin/poke", shell=True)
 
 print("Cleaning up after myself...")
+call("rm -r pyinstall pyinstall.tar.gz  dist/ build/ poke.spec source/*.pyc", shell=True)
 
-call("rm -r pyinstall pyinstall.tar.gz  dist/ build/ poke.spec", shell=True)
-
+print("\n")
 check = raw_input("Do you want me to link the '/usr/bin' to your path (if not already)? [Y/n]: ")
 
 if check.lower() == "y" or check.lower() == "":
 	path = environ['PATH'].split(pathsep)
 	if not'/usr/bin' in path:
 		call("export PATH='$PATH:/usr/bin'", shell=True)
-	else:
-		print "/usr/bin/ is alrady linked in path. Omitting export..."	
 else:
 	print "Not linking directory..."
 
+print("\n")
+print "Would you like to move the source files of Poke to a more convenient location? (/usr/local/src)"
+source = raw_input("You will be able to re-compile Poke from there or remove it if you so desire without having to download any additional files. [Y/n]: ")
+
+if source.lower() == "n":
+	pass
+else:
+	call("sudo mkdir -p /usr/local/src", shell=True)
+	call("cd .. && sudo cp -r Poke /usr/local/src/poke", shell=True)
+
 time = (datetime.now()-startTime)
+print("\n")
 print("The script is now finished. I took %s to run. Type 'poke' to configure the application" % time)
 exit()
--- remove.py
+++ remove.py
@@ -1,12 +1,40 @@
 #!/usr/bin/python
 
+'''
+
+Copyright (c) 2014 Katharina Sabel <katharina.sabel@2rsoftworks.de>
+Copyright (c) 2014 Random Robot Softworks
+www.katharinasabel.de | www.2rsoftworks.de
+
+Licensed under the Apache License, Version 2.0 (the "License");		
+you may not use this file except in compliance with the License.		
+You may obtain a copy of the License at	
+
+		http://www.apache.org/licenses/LICENSE-2.0						
+									
+Unless required by applicable law or agreed to in writing, software	
+distributed under the License is distributed on an "AS IS" BASIS,		
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Found a bug? Report it in the repository issue tracker:
+
+		https://github.com/SpaceKookie/Poke
+
+'''
+
+
 from datetime import datetime
 from subprocess import call
 from sys import exit
 
+binary = False
+source = False
+config = False
+
 print("This will remove poke from your /usr/bin directory.")
-print("This will *not* remove your configuration files from ~/.poke")
-note = "Note you will have to execute this script with root privileges to install poke correctly. Continue? [Y/n]: "
+note = "Note you will have to execute this script with root privileges to remove poke correctly. Continue? [Y/n]: "
 
 usrInput = raw_input(note)
 
@@ -22,5 +50,33 @@ else:
 	exit()
 
 call("sudo rm /usr/bin/poke", shell=True)
+binary = True
 
-print("Removal complete.")
+print("")
+askConfig = raw_input("Do you want to remove your config files from ~/.poke/? [Y/n]: ")
+
+if askConfig.lower() == "n":
+	print("Skipping config files")
+	pass
+else:
+	call("rm -r ~/.poke/", shell=True)
+	config = True
+
+print ("")
+askSource = raw_input("Do you want to remove the source-files from /usr/local/src/poke? (May include this script!) [Y/n]: ")
+
+if askSource.lower() == "n":
+	print("Skipping source files")
+	pass
+else:
+	call("sudo rm -r /usr/local/src/poke", shell=True)
+	source = True
+
+if source and binary and config:
+	print("Poke is now no longer installed on your system. I hope you're happy...")
+elif binary and source:
+	print("Poke binary and source are no longer on your machine. Config files are still present under ~/.poke")
+elif binary and config:
+	print("Poke binary and config are no longer on your machine. But you can always recompile the tool from source at /usr/local/src/poke")
+else:
+	print "Poke is still installed."
