c769f7189231809ab0df0503ce539ce692a13a29
Katharina Sabel <sabel.katharina@gmail.com>
2015-12-18 11:28:48 +0100

    Making docopt real pretty

--- poke/poke.py
+++ poke/poke.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 
-# This is the help screen
-"""
-"""
+"""___
+\  \ 
+ \  \ Poke 
+ /  /     ssh connection utility
+/__/
 
-# Now make the help screen
-import docopt
+Usage:
+  poke (--mkconf | -c)
+  poke <server>
+  poke -q <server>
+  poke (-h | --help)
+  poke --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  <server>      Connect to that server from your ssh config
+  -q <server>   Connect to that server without copying files from .pokerc
+  --mkconf -c   Add a new entry to your ssh config from your shell history
+
+"""
+from docopt import docopt
 
 # Make sure we import all the things we need to namespaces we like
 from helpers import ConfigHelper, ShellHelper, ConnectHelper, CmdParser
@@ -33,4 +49,6 @@ class Poker:
     pass
 
 if __name__ == '__main__':
-  p = Poker()
+  arguments = docopt(__doc__, version='0.7.0')
+  print(arguments)
+  # p = Poker()
