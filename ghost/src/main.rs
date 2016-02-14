e0a67a457eb3cd8d066e8943de3f38222d7aacb0
Katharina Sabel <sabel.katharina@gmail.com>
2016-02-14 02:41:12 +0100

    Adding back functionality of the python version (after the reboot of the project). Now 100% more rustic...

--- ghost/src/main.rs
+++ ghost/src/main.rs
@@ -1,54 +1,27 @@
-// External includes
-/*use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
-
-use std::io::BufReader;*/
-
-// Internal includes
-
-/*fn main() {
-
-  /* Create a path to the desired file */
-  let path = Path::new("/home/spacekookie/.ssh/config");
-  let display = path.display();
-
-  /* The `description` method of `io::Error` returns a string that describes the error */
-  let file = match File::open(&path) {
-    Err(why) => panic!("couldn't open {}: {}", display, Error::description(&why)),
-    Ok(file) => file,
-  };
-
-  /* Iterate over the lines and print them */
-  let br = BufReader::new(&file);
-  for line in br.lines() {
-    let l = line.unwrap();
-
-    if l.trim().starts_with("HostName") {
-      // print!("\t\t {}\n", &l[12..]);
-    } else if l.trim().starts_with("Host") {
-      print!("- {}\n", &l[5..]);
-    } else if l.trim().starts_with("User")
-    // println!("> {}", l);
-  }
-}
-*/
 extern crate rustc_serialize;
 extern crate docopt;
 
+/** External imports */
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::error::Error;
+use std::path::Path;
+use std::fs::File;
+
 use docopt::Docopt;
 
+/* Internal imports */
+
+
+/* Docopt definitions */
 const VERSION: &'static str = "0.7.0";
-
 const USAGE: &'static str = "Ghost in the Shell
-
 Usage:
-  ghost <server> <ssh command>
-  ghost (add | a) <server>
-  ghost rm <server>
-  ghost [-q] <server> [ssh options]
-  ghost (list | ls)
+  ghost --add <server> <sshcommand>
+  ghost --add <server>
+  ghost --rm <server>
+  ghost [-q] <server>
+  ghost --list
   ghost (-h | --help)
   ghost (--version)
 
@@ -60,30 +33,67 @@ Options:
   ls              Show all targets from ssh config
   <server>        Connect to that server from your ssh config
 ";
-//  -q <server>   Connect to that server without copying files from .pokerc
 
 #[derive(Debug, RustcDecodable)]
 struct Args {
-
-    /* Additional flags */
     flag_q: bool,
-    flag_version: bool,
+    flag_add: bool,
+    flag_rm: bool,
+    flag_list: bool,
 
-    /* Arguments */
     arg_server: Vec<String>,
-    arg_x: Option<i32>,
-    arg_y: Option<i32>,
-
-    /* Commands */
-    cmd_add: bool,
-    cmd_list: bool,
-    cmd_rm: bool,
+    arg_sshcommand: Vec<String>,
 }
 
+struct ConfigHelper<'a> {
+    ssh_path: &'a str,
+}
+
+impl<'a> ConfigHelper<'a> {
+    pub fn new(ssh_path: &str) -> ConfigHelper {
+        ConfigHelper {
+            ssh_path: ssh_path,
+        }
+    }
+
+    fn parseConfig(&self) {
+        /* The `description` method of `io::Error` returns a string that describes the error */
+        let file = match File::open(self.ssh_path) {
+          Err(why) => panic!("couldn't open {}: {}", self.ssh_path, Error::description(&why)),
+          Ok(file) => file,
+        };
+
+        /* Iterate over the lines and print them */
+        let br = BufReader::new(&file);
+        for line in br.lines() {
+            let l = line.unwrap();
+
+            if l.trim().starts_with("HostName") {
+                // print!("\t\t {}\n", &l[12..]);
+            } else if l.trim().starts_with("Host") {
+                print!("- {}\n", &l[5..]);
+            } else if l.trim().starts_with("User") {
+                // println!("> {}", l);
+            }
+        }
+    }
+}
+
+/* Main entry point for our application */
 fn main() {
-  let version = VERSION.to_owned();
-  let args = Docopt::new(USAGE).and_then(|dopt| dopt.version(Some(version)).parse()).unwrap_or_else(|e| e.exit());
+    let version = VERSION.to_owned();
+    // let args = Docopt::new(USAGE).and_then( |dopt| dopt.version(Some(version)).parse() ).unwrap_or_else( |e| e.exit() );
+    let args: Args = Docopt::new(USAGE).and_then(|d| d.version(Some(version)).decode()).unwrap_or_else(|e| e.exit());
 
-  println!("Some arguments:");
-  println!("  Speed: {}", args.get_str("--speed"));
+    if args.flag_list {
+      let cfg = ConfigHelper::new("/home/spacekookie/.ssh/config");
+      cfg.parseConfig();
+    } 
 }
+
+/*    println!("\nSome values:");
+    println!("  Server {:?}", args.arg_server);
+    println!("  SSH CMD {:?}", args.arg_sshcommand);
+    println!("  Invoking q: {:?}", args.flag_q);
+    println!("  Invoking add: {:?}", args.flag_add);
+    println!("  Invoking rm: {:?}", args.flag_rm);*/
