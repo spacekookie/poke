a248c2fb3bdf1cd68f1cf08336a792c02f2752e7
Katharina Sabel <sabel.katharina@gmail.com>
2016-12-04 01:47:15 +0100

    Adding some code. Whoop whoop

--- include/poke.h
+++ include/poke.h
@@ -1,12 +1,7 @@
 /* libpoke - poke.h
  *
- * This file defines a public API for a binary interface to bind
- *   against. It extends most base functionality of poke as well as
- *   hooks to bind new functions into either at compile time (C)
- *   or via a scripting file.
- *
- * (c) 2016 					Katharina Sabel.
- * Authors:						Katharina 'spacekookie' Sabel
+ * (c) 2016           Katharina Sabel.
+ * Authors:           Katharina 'spacekookie' Sabel
  *
  * This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -18,13 +13,51 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
- * -------------------------------------------
- *
  */
 
+/**************************************************************************************/
+
+
+#define PK_ERR_SUCCESS          0
+#define PK_ERR_GENERROR         1
+#define PK_ERR_MALLOC_FAILED    2
+#define PK_ERR_INVALID_PARAMS   3
+
+
+/* Base data struct to represent a server in the ssh config */
+typedef struct pk_trgt_ctx
+{
+  char *hostname;
+  char *username;
+  char *key_id;
+  
+  long last_updated;
+};
+
+/** Initialise a new pk target with a hostname and username, kid can be null */
+int pk_trgt_init(pk_trgt_ctx *ctx, const char *hst_n, const char *usr_n, char *kid);
+
+/** Changes the key ID of the provided context and updates the access time */
+int pk_trgt_update(pk_trgt_ctx *ctx, char *kid);
+
+/** Cleanly de-allocates all the resources from the context */
+int pk_trgt_free(pk_trgt_ctx *ctx);
+
+/**************************************************************************************/
 
 /** Main poke daemon context struct */
 typedef struct pk_dm_ctx
 {
-	
+  const char      *cfg_pth;
+  long            start_t;
+  pk_trgt_ctx     **targets;
 };
+
+/** Initialises a context with everything that is required for it to work **/
+int pk_dm_init(pk_dm_ctx *ctx, const char *cfg_pth);
+
+/** Queries target information from the known hosts. Can replace ssh **/
+int pk_dm_query(pk_dm_ctx *ctx, pk_trgt_ctx **trgt, const char *host);
+
+/** Safely frees a context from memory **/
+int pk_dm_free(pk_dm_ctx *ctx);
