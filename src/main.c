755442a3d74cf561a6ce23b5d2529a0d028cd035
Katharina Sabel <sabel.katharina@gmail.com>
2017-03-13 01:11:24 +0100

    You can now easily generate keys and connect to servers. You still need to manually copy key IDs

--- src/main.c
+++ src/main.c
@@ -4,11 +4,16 @@
 #include <stdlib.h>
 
 #include <argp.h>
+#include <wordexp.h>
+
+//#define SSH_PATH "/home/spacekookie/.ssh/config"
+
+int poke_connect(pk_parse_hst *host);
+int poke_genkey(pk_parse_hst *host);
 
 /* Declare const strings with metadata */
 const char *argp_program_version = "Poke 0.6b";
 const char *argp_program_bug_address = "<kookie@spacekookie.de>";
-
 
 static char doc[] = "Poke -- a powerful ssh utility";
 static char args_doc[] = "SERVER";
@@ -57,43 +62,43 @@ int main (int argc, char **argv)
     /* Print out the options and stuff */
     printf ("Server = %s\nGenerate = %s\n", arguments.server, arguments.generate ? "yes" : "no");
 
+    wordexp_t exp_result;
+    wordexp("~/.ssh/config", &exp_result, 0);
+    printf("SSH Path: %s\n", exp_result.we_wordv[0]);
+
+    /* Create a context for a test path */
+    pk_parse_ctx parser;
+
+    /* Parse the ssh config */
+    pk_parse_init(&parser, exp_result.we_wordv[0]);
+    pk_parse_load(&parser);
+
+    pk_parse_hst *host;
+    int err = pk_parse_query(&parser, &host, arguments.server);
+    if(err) {
+        printf("Server %s not found in config %s!\n", arguments.server, exp_result.we_wordv[0]);
+        return 255;
+    }
+
+    if(arguments.generate) {
+        poke_genkey(host);
+    } else {
+        poke_connect(host);
+    }
+
+    pk_parse_free(&parser);
     exit (0);
 }
 
+int poke_connect(pk_parse_hst *host)
+{
+    char cmd[256];
+    memset(cmd, 0, 256);
+    sprintf(cmd, "ssh %s", host->host_id);
 
-
-
-//    /* Check if we were given a "host" */
-//    if(argn < 1) {
-//        printf("No host provided\n");
-//        return 255;
-//    }
-//
-//    /* Copy a reference to the provided hostname */
-//    char hostname[256];
-//    memset(hostname, 0, sizeof(char) * 256)
-//    strcpy(hostname, argv[1]);
-//
-//    /* Create a context for a test path */
-//    pk_parse_ctx parser;
-//
-//    /* Parse the ssh config */
-//    pk_parse_init(&parser, SSH_PATH);
-//
-//    pk_parse_load(&parser);
-//
-//    pk_parse_hst *host;
-//    int err = pk_parse_query(&parser, &host, hostname);
-//
-//    /* If the host was null */
-//    if(host == NULL) {
-//        printf("The host provided wasn't found in %s\n", SSH_PATH);
-//        return 155;
-//    }
-//
-//    /* Free resources */
-//    pk_parse_free(&parser);
-//    return 0;
+    system(cmd);
+    return 0;
+}
 
 int poke_genkey(pk_parse_hst *host)
 {
@@ -103,7 +108,5 @@ int poke_genkey(pk_parse_hst *host)
 
     /* Execute command */
     system(cmd);
-
-
     return 0;
 }
