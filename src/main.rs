14082f6c652556edcc9385ebd6a540220392683b
Katharina Fey <kookie@spacekookie.de>
2018-04-14 18:35:19 +0200

    Tweaking on argument parsing

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,26 @@ use clap::{App, Arg, SubCommand};
 use std::fs;
 use std::path::PathBuf;
 
-fn get_ssh_dir() -> PathBuf {
-    let mut base = PathBuf::from(std::env::home_dir().unwrap());
-    base = base.join(".ssh").join("poke");
-    return base;
-}
+const APP_NAME: &'static str = env!("CARGO_PKG_NAME");
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const ABOUT: &'static str = env!("CARGO_PKG_DESCRIPTION");
+const AUTHOR: &'static str = env!("CARGO_PKG_AUTHORS");
 
 fn main() {
-    let app = App::new("poke")
-        .version("0.0.0")
-        .about("Powerful utility to manage ssh keys")
+    let m = App::new(APP_NAME)
+        .version(VERSION)
+        .about(ABOUT)
+        .author(AUTHOR)
         .subcommand(
             SubCommand::with_name("generate")
                 .alias("g")
                 .about("Generate new keys for this computer for a specific server")
-                .arg(Arg::with_name("name").required(true))
-                .arg(Arg::with_name("addr").required(true)),
-        );
+                .arg(Arg::with_name("name").required(true).help("Provide a name for the new generated keyfile"))
+                .arg(Arg::with_name("addr").required(true).help("Provide the remote address for your server")),
+        )
+        .get_matches();
 
-    match app.get_matches().subcommand() {
+    match m.subcommand() {
         ("generate", Some(m)) => {
             if !get_ssh_dir().exists() {
                 fs::create_dir_all(get_ssh_dir()).unwrap();
@@ -41,6 +42,12 @@ fn main() {
                 &addr,
             );
         }
-        _ => {}
+        _ => println!("Missing arguments: type `poke --help` for more help!"),
     }
+}
+
+fn get_ssh_dir() -> PathBuf {
+    let mut base = PathBuf::from(std::env::home_dir().unwrap());
+    base = base.join(".ssh").join("poke");
+    return base;
 }
