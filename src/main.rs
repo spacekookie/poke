dfcc793103dd76c66c07787b2e17ab6c1ab3ae1f
Katharina Fey <kookie@spacekookie.de>
2018-04-14 15:50:13 +0200

    Making the first key generation work

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
 extern crate clap;
 
-use clap::{App, SubCommand};
+mod ssh;
+
+use clap::{App, Arg, SubCommand};
+use std::fs;
+use std::path::PathBuf;
+
+fn get_ssh_dir() -> PathBuf {
+    let mut base = PathBuf::from(std::env::home_dir().unwrap());
+    base = base.join(".ssh").join("poke");
+    return base;
+}
 
 fn main() {
     let app = App::new("poke")
         .version("0.0.0")
-        .author("Katharina Fey")
         .about("Powerful utility to manage ssh keys")
         .subcommand(
             SubCommand::with_name("generate")
                 .alias("g")
-                .about("Generate new keys for this computer for a specific server"),
+                .about("Generate new keys for this computer for a specific server")
+                .arg(Arg::with_name("name").required(true)),
         );
 
-    match app.get_matches() {
-        _ => unreachable!("The compiler should prevent this."),
+    match app.get_matches().subcommand() {
+        ("generate", Some(m)) => {
+            if !get_ssh_dir().exists() {
+                fs::create_dir_all(get_ssh_dir()).unwrap();
+            }
+
+            let name = m.value_of("name").unwrap();
+            ssh::generate_key(get_ssh_dir().to_str().unwrap(), name);
+        }
+        _ => {}
     }
 }
--- src/ssh.rs
+++ src/ssh.rs
@@ -3,7 +3,16 @@
 use std::process::Command;
 
 /// Generate a new key and store it in $USER/.ssh/poke
-pub fn generate_key(name: &str) {
-    let foo = Command::new("ssh-keygen").args($["-t", "ed25519", "-f", &format!("$HOME/.ssh/{}", name), "-N", "''"]).output()
-            .expect("Failed to execute `ssh-keygen`");
+pub fn generate_key(path: &str, name: &str) {
+    let foo = Command::new("ssh-keygen")
+        .arg("-t")
+        .arg("ed25519")
+        .arg("-f")
+        .arg(&format!("{}/{}", path, name))
+        .arg("-N")
+        .arg("''")
+        .output()
+        .expect("Failed to execute `ssh-keygen`");
+
+    print!("{}", String::from_utf8(foo.stdout).unwrap());
 }
